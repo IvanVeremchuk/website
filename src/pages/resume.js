fc41c88c2a595ed2927960f1e3a53945d8162495
Ivan Veremchuk <yahoo66x@gmail.com>
2024-01-09 20:28:44 -0500

    Update resume.js

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -7,15 +7,6 @@ function Resume() {
         <>
             <div className="container-resume">
                 <div className='resume'>
-                    <a className='button-link' href="https://drive.google.com/file/d/1M5geKwATnPEBa2oW1lE5DqQ-H9WULw6W/view?usp=sharing" download="IvanVeremchukResume.pdf">
-                        Download PDF
-                    </a>
-                    <a className='button-link' href="https://docs.google.com/document/d/19zcUQhyJZf0b5kIfLFJDqmQQnZICSigH/edit?usp=sharing&ouid=101388727716091087802&rtpof=true&sd=true" download="IvanVeremchukResume.docx">
-                        Download docx
-                    </a>
-                    <a className='button-link' href="https://drive.google.com/file/d/18CHOlzdgWoR_tR_oUDcVb8-VA7aq-H9K/view?usp=sharing" download="IvanVeremchukResume.txt">
-                        Download txt
-                    </a>
                 </div>
                 <hr className='x'></hr>
                 <div className="image-c">
