6e49963fddbc277068159e1c92b3bc5354a70e3b
Ivan Veremchuk <yahoo66x@gmail.com>
2024-01-09 20:29:35 -0500

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,10 +8,6 @@ function About() {
           <div className='parent'>
             <div className='child-image'></div>
             <div className='child-p'>
-              <h2>What I’m all about</h2>
-              <p>
-                    I believe in time management and discipline. I created my first hand-coded webpage in 2016. My approach is simple: to make something, make it fast, make it now, make it by yourself and it is never too late to start doing it. I am like & passionate about fitness, martial arts, drawing and Debian Linux.
-              </p>
             </div>
           </div>
         </>
