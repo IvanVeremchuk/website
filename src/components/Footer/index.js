05d31e43b2badca154e2e9e3eace18287d1979e7
Ivan Veremchuk <yahoo66x@gmail.com>
2024-01-09 20:55:27 -0500

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,13 +7,13 @@ const Navbar = () => {
       <a className="footer-link" href="https://github.com/IvanVeremchuk" target="_blank" rel="noreferrer" >
         <span class="fa-brands fa-github fa-xl" />
       </a>
-      <a className="footer-link" href="https://t.me/Ivan_w" target="_blank" rel="noreferrer">
+      <a className="footer-link" href="https://proton.me/mail" target="_blank" rel="noreferrer">
         <span class="fa-regular fa-paper-plane fa-xl" />
       </a>
       <a className="footer-link" href="https://www.linkedin.com/in/ivan-veremchuk" target="_blank" rel="noreferrer" >
         <span class="fa-brands fa-linkedin-in fa-xl" />
       </a>
-      <a className="footer-link" href="href=mailto:ivanveremchukdev@gmail.com">
+      <a className="footer-link" href="href=mailto:eduard.ivan@proton.me">
         <span class="fa-regular fa-envelope fa-xl" />
       </a>
     </nav>
